ba46d1ffeab8b2da2bcbf0ba326aa3d14afb54ba
root <t.uwabera@alustudent.com>
2023-05-16 09:23:42 -0700

    h

--- regular_expressions/1-repetition_token_0.rb
+++ regular_expressions/1-repetition_token_0.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
 # Print the concatenated string and its length
 #puts "#{concatenated_string} (#{concatenated_string.length} chars long)"
-puts ARGV[0].match(/hbt{2,5}n/)i
+puts ARGV[0].match(/hbt{2,5}n/)
 
