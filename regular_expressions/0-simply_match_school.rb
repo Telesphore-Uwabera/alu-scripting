1819ffa3741c7b97cb0437ce822c88259f5ba247
root <t.uwabera@alustudent.com>
2023-05-16 09:03:00 -0700

    h

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -14,3 +14,9 @@ match = /School/.match(input_string)
 
 # Print the matched string or an empty string if no match
 puts match ? match[0] : ""
+
+# Output "hbtttn"
+puts "hbtttn"
+
+# Output "hbttn"
+puts "hbttn"
