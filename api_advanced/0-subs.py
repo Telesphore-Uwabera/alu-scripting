3abde1ce132a7347b5c5af7e825fc37c0ffdd850
Telesphore-Uwabera <t.uwabera@alustudent.com>
2023-07-12 12:37:01 +0000

    :

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,19 +1,45 @@
 #!/usr/bin/python3
+
+"""
+Queries the Reddit API and returns the number of subscribers for a given subreddit.
+If the subreddit is invalid, returns 0.
+"""
+
 import requests
 
 def number_of_subscribers(subreddit):
     """
-    Queries the Reddit API and returns the number of subscribers for a given subreddit.
-    If an invalid subreddit is given, the function returns 0.
+    Returns the number of subscribers for a given subreddit.
+    If the subreddit is invalid, returns 0.
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    headers = {"User-Agent": "Custom User Agent"}  # Set a custom User-Agent header
 
+    response = requests.get(url, headers=headers)
     if response.status_code == 200:
-        data = response.json()
-        subscribers = data["data"]["subscribers"]
-        return subscribers
+        return response.json().get("data", {}).get("subscribers", 0)
+    else:
+        return 0
+#!/usr/bin/python3
+
+"""
+Queries the Reddit API and returns the number of subscribers for a given subreddit.
+If the subreddit is invalid, returns 0.
+"""
+
+import requests
+
+def number_of_subscribers(subreddit):
+    """
+    Returns the number of subscribers for a given subreddit.
+    If the subreddit is invalid, returns 0.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Custom User Agent"}  # Set a custom User-Agent header
+
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json().get("data", {}).get("subscribers", 0)
     else:
         return 0
 
