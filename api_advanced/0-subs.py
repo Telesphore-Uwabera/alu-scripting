2b93df194120a71a3e181351af03e069ddbfee7a
TelesphoreUwabera <t.uwabera@alustudent.com>
2023-07-12 20:37:27 +0200

    Update 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -2,6 +2,12 @@
 
 """
 0-subs
+
+Queries the Reddit API and returns the number of subscribers for a given subreddit.
+If not a valid subreddit, return 0.
+
+Author: Telesphore Uwabera
+Date: July 12, 2023
 """
 
 import requests
@@ -10,6 +16,20 @@ def number_of_subscribers(subreddit):
     """
     Queries the Reddit API and returns the number of subscribers for a given subreddit.
     If not a valid subreddit, return 0.
+
+    Args:
+        subreddit (str): The name of the subreddit to query.
+
+    Returns:
+        int: The number of subscribers for the subreddit.
+            Returns 0 if the subreddit is invalid or an error occurs.
+
+    Raises:
+        None
+
+    Example Usage:
+        >>> number_of_subscribers('programming')
+        756024
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'Mozilla/5.0'}  # Set a custom User-Agent header
@@ -22,4 +42,3 @@ def number_of_subscribers(subreddit):
         return subscribers
     else:
         return 0
-
