e3c6c9854bd252d3fcf74edd3af1f9292547596a
TelesphoreUwabera <t.uwabera@alustudent.com>
2023-07-14 04:01:33 +0200

    Update 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,44 +1,15 @@
 #!/usr/bin/python3
-
-"""
-0-subs
-
-Queries the Reddit API and returns the number of subscribers for a given subreddit.
-If not a valid subreddit, return 0.
-
-Author: Telesphore Uwabera
-Date: July 12, 2023
-"""
-
+""" 0-subs module """
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number of subscribers for a given subreddit.
-    If not a valid subreddit, return 0.
-
-    Args:
-        subreddit (str): The name of the subreddit to query.
-
-    Returns:
-        int: The number of subscribers for the subreddit.
-            Returns 0 if the subreddit is invalid or an error occurs.
-
-    Raises:
-        None
-
-    Example Usage:
-        >>> number_of_subscribers('programming')
-        756024
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0'}  # Set a custom User-Agent header
-
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
+    """ returns the number of subscribers """
+    url = "https://www.reddit.com/r/{}/about.json" \
+        .format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get(url, headers=headers)
     if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
-        return 0
+        return response.json().get('data') \
+            .get('subscribers')
+    return 0
