1ce81ddf60c9270a939ca70f16c2cb417fcad286
TelesphoreUwabera <t.uwabera@alustudent.com>
2023-07-14 04:23:52 +0200

    Update 3-count.py

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -20,26 +20,26 @@ def count_words(subreddit, word_list, after='', hot_list=None):
         for topic in data['data']['children']:
             title_words = topic['data']['title'].split()
             for word in title_words:
-                for i in range(len(word_list)):
-                    if word_list[i].lower() == word.lower():
+                for i, keyword in enumerate(word_list):
+                    if keyword.lower() == word.lower():
                         hot_list[i] += 1
 
         after = data['data']['after']
         if after is None:
             word_counts = {}
             for i, word in enumerate(word_list):
-                if i not in word_counts:
-                    word_counts[i] = hot_list[i]
+                if word not in word_counts:
+                    word_counts[word] = hot_list[i]
                 else:
-                    word_counts[i] += hot_list[i]
+                    word_counts[word] += hot_list[i]
 
             sorted_counts = sorted(
                 word_counts.items(),
-                key=lambda x: (-x[1], word_list[x[0]].lower())
+                key=lambda x: (-x[1], x[0].lower())
             )
 
-            for i, count in sorted_counts:
+            for word, count in sorted_counts:
                 if count > 0:
-                    print("{}: {}".format(word_list[i].lower(), count))
+                    print("{}: {}".format(word.lower(), count))
         else:
             count_words(subreddit, word_list, after, hot_list)
