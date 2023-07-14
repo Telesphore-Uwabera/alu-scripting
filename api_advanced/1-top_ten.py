79fc1b4b478c60d5946f1089994ffed153003611
TelesphoreUwabera <t.uwabera@alustudent.com>
2023-07-14 04:02:34 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,29 +1,18 @@
 #!/usr/bin/python3
-
-"""
-0-subs
-
-Queries the Reddit API and returns the number of subscribers for a given subreddit.
-If not a valid subreddit, return 0.
-
-Author: Your Name
-Date: July 12, 2023
-"""
+"""Module documentation"""
 import requests
 
+
 def top_ten(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-Agent': 'Mozilla/5.0'}  # Set a custom User-Agent header
-    
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    
+    """Function that queries the Reddit API."""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10" \
+        .format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get(url, headers=headers)
+
     if response.status_code == 200:
-        data = response.json()
-        posts = data['data']['children']
-        
-        for post in posts:
-            title = post['data']['title']
-            print(title)
+        data = response.json().get('data').get('children')
+        for post in data:
+            print(post.get('data').get('title'))
     else:
         print(None)
-
